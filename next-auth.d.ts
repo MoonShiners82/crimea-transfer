c8195220e8862b4206e92709f8b9f55685e869ed
Сутягин Владимир Николаевич <vnsutyagin@crki.local>
2026-07-02 14:06:04 +0300

    Fix TypeScript types

---
+++ next-auth.d.ts
@@ -0,0 +1,21 @@
+﻿import { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      phone?: string | null
+      role?: string | null
+    } & DefaultSession["user"]
+  }
+  interface User {
+    phone?: string | null
+    role?: string | null
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    phone?: string | null
+    role?: string | null
+  }
+}
